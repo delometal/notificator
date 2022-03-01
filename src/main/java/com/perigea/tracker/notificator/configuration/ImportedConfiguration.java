e80c694af732b8036a1ff6e85ada3b594f0f28dc
samuelgen11 <samuelgen11@gmail.com>
2022-03-01 11:37:07 +0100

    add multithreading

--- src/main/java/com/perigea/tracker/notificator/configuration/ImportedConfiguration.java
+++ src/main/java/com/perigea/tracker/notificator/configuration/ImportedConfiguration.java
@@ -1,10 +1,19 @@
 package com.perigea.tracker.notificator.configuration;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration("commonsConfiguration")
-@ComponentScan(basePackages = {"com.perigea.tracker.commons"})
+@ComponentScan(basePackages = { "com.perigea.tracker.commons" })
 public class ImportedConfiguration {
 
+	@Bean
+	public ThreadPoolExecutor threadExecutor() {
+		return (ThreadPoolExecutor) Executors.newFixedThreadPool(20);
+
+	}
 }
--- src/main/java/com/perigea/tracker/notificator/rest/NotificationController.java
+++ src/main/java/com/perigea/tracker/notificator/rest/NotificationController.java
@@ -1,25 +1,56 @@
 package com.perigea.tracker.notificator.rest;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.perigea.tracker.commons.dto.ResponseDto;
 import com.perigea.tracker.commons.model.Email;
 import com.perigea.tracker.notificator.email.EmailSender;
 
 @RestController
 public class NotificationController {
-	
+
 	@Autowired
 	private EmailSender emailClient;
+
+	@Autowired
+	private Logger logger;
 	
-	@PostMapping(path = "send_email") 
-	public ResponseEntity<String> sendEmail(@RequestBody Email email) {
-		System.out.println(email.toString());
-		emailClient.mandaNotifica(email);
-		return new ResponseEntity<>("Email mandata", HttpStatus.OK);
+	@Autowired
+	private ThreadPoolExecutor threadExecutor;
+	
+	@PostMapping(path = "send_email")
+	public ResponseEntity<ResponseDto<String>> sendEmail(@RequestBody Email email) {
+		
+		threadExecutor.execute(()->{
+			logger.info(email.toString());
+			emailClient.mandaNotifica(email);
+		});
+//		Thread controller = new Thread(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				logger.info(email.toString());
+//				emailClient.mandaNotifica(email);
+//				try {
+//					Thread.sleep(5000);
+//					logger.info("Thread interrupted");
+//				} catch (InterruptedException e) {
+//					return;
+//				}
+//			}
+//
+//		});
+//		controller.run();
+
+		ResponseDto<String> genericDto = ResponseDto.<String>builder().data("Email mandata con successo").build();
+		return ResponseEntity.ok(genericDto);
+
 	}
 }
